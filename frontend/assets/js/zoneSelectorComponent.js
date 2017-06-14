870a72b3bdbb5f6cdd5ce0848c411f333cee5d1f
Evan Sailer <esailer@asu.edu>
2017-06-13 20:00:11 -0500

    removed unused code

--- frontend/assets/js/zoneSelectorComponent.js
+++ frontend/assets/js/zoneSelectorComponent.js
@@ -4,7 +4,6 @@ Vue.component('zone-selector-component', {
   template: '<div class="zonebox"><h3>Zones</h3><div v-for="zone in zones"><label class="context-menu--select--label">{{zone.label}}:<select class="context-menu--select--select" v-model="zone.value" @change="changeZone"><option v-for="choice in zone.choices" v-bind:value="choice.code">{{ choice.name }}</option></select></label></div></div>',
   methods: {
     changeZone: function () {
-        console.log('triggered');
       this.$emit('zone');
     }
   },
